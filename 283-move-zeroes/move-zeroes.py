7c129cf24ea0a4a6f60af0ad37de92afac6ae273
Vaibhavi2311 <vaibhavisawant2311@gmail.com>
2026-09-13 18:46:27 +0530

    Time: 3 ms (81.73%) | Memory: 20.5 MB (62.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,13 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        '''l=0
+        l=0
         for r in range(len(nums)):
             if nums[r]:
                 nums[l],nums[r]=nums[r],nums[l]
                 l+=1
-        return nums'''
-        n=len(nums)
+        return nums
+        '''n=len(nums)
         temp=[]
         for i in range(0,n):
            if nums[i]!=0:
@@ -18,5 +18,5 @@ class Solution:
         for i in range(0,nz):
             nums[i]=temp[i]
         for i in range(nz,n):
-            nums[i]=0
+            nums[i]=0'''
         
